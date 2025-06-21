2e58c3b0ff1d046eb35a14d3c006fdad90aa1f32
Kristian Mølbach Lian <molbachlian@gmail.com>
2025-06-22 01:06:51 +0200

    Further work. Made API requests work, but they are very slow. A static image host is required

--- src/main.py
+++ src/main.py
@@ -32,18 +32,41 @@ def create_user():
 def get_image_categories():
     base_dir = os.path.join(app.static_folder, 'images')
 
+
     if not os.path.isdir(base_dir):
         return jsonify({"error": "Image folders not found"}), 404
 
     image_categories = [os.path.basename(category) for category in os.listdir(base_dir)]
 
+    # Looping through each category. Each category is a JSON object in the output
     data = []
+    image_file_iter = 0
+    for i, image_category_name in enumerate(image_categories):
 
-    for i, image_category in enumerate(image_categories):
+        image_category_path = url_for('static', filename=f'images/{image_category_name}/')
+        image_category_file_urls = [image_category_path + file_name for file_name in os.listdir(os.path.join(base_dir, image_category_name))]
+
+        # Filling each category with it's respective image URIs and a unique image ID for each image
+        category_image_objects = []
+        for image_category_file_url in image_category_file_urls:
+
+            # One 'category_image_object' has a unique image_id, and a URI
+            category_image_objects.append({
+                "image_id": str(image_file_iter),
+                "image_url": f'http://{os.getenv("computer_LAN_IP")}:{os.getenv("host_port")}' + image_category_file_url
+            })
+
+            image_file_iter += 1
+
+        # Appending each folder's files to an output data object
         data.append({
             "category_id": str(i + 1),
-            "image_category": image_category
+            "image_category": image_category_name,
+            "images": category_image_objects
         })
+
+    print(data)
+
 
     return jsonify(data), 200
 
@@ -70,7 +93,6 @@ def get_images_per_category(category):
             "uri": f'http://{os.getenv("computer_LAN_IP")}:{os.getenv("host_port")}' + image_url
         })
 
-    print(jsonify(data).data)
     return jsonify(data), 200
 
 
@@ -115,4 +137,4 @@ def update_training_data():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host=os.getenv("computer_LAN_IP"), port=os.getenv("host_port"))  # Use environment variables for IP and port
+    app.run(host=os.getenv("computer_LAN_IP"), port=os.getenv("host_port"))  # Use environment variables for IP and port
